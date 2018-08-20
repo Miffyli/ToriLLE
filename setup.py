8a1187668df36ef861eb89986fcaded4d1ecbc2f
Anssi 'Miffyli' Kanervisto <kaneran21@hotmail.com>
2018-08-20 11:18:20 +0300

    [pypi] Updated setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="torille",
-    version="0.9.2",
+    version="0.9.3",
     author="Anssi 'Miffyli' Kanervisto",
     author_email="anssk@cs.uef.fi",
     description="Toribash as an agent learning environment",
